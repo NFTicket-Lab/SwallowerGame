58243bf932e3d71d83c64ac17555e1d6649a2ac8
kaiweicai <kaiweicai@163.com>
2022-01-18 19:49:23 +0800

    add account.cs

---
+++ Assets/Scripts/Accounts.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Accounts : MonoBehaviour{
+    public Text Mint1Text;
+
+    public string mintname;
+    // Start is called before the first frame update
+    void Start(){
+        StartCoroutine(IMintEvent(mintname));
+    }
+
+    IEnumerator IMintEvent(string name) {
+        string url = "http://localhost:5000/api/metaData";
+        WWW www = new WWW(url);
+        yield return www;
+        if (!www.isDone){
+            Debug.Log("wait for response!");
+        } else {
+            Mint1Text.text = www.text;
+            Debug.Log(www.text);
+        }
+    }
+}
