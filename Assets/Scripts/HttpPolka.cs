83915993bf857e561019d3fcec44815f4446a90c
kaiweicai <kaiweicai@163.com>
2022-01-19 20:07:15 +0800

    add gitignore

--- Assets/Scripts/HttpPolka.cs
+++ Assets/Scripts/HttpPolka.cs
@@ -11,7 +11,7 @@ public class HttpPolka : MonoBehaviour
 
     public Text MintText;
 
-    public List<string> MineKeys;
+    public List<string> accountKeys;
 
 
     public Text CurrPubKeyText;
@@ -24,7 +24,7 @@ public class HttpPolka : MonoBehaviour
     public Button MintBtn;
     public InputField inputMintName;
 
-    public InputField mintMesInput;
+    public InputField chainMessageZone;
     private void Awake()
     {
         if (Instance == null) {
@@ -40,18 +40,23 @@ public class HttpPolka : MonoBehaviour
             });
         }
 
-        if (MineKeys.Count > 0)
-        {
-            CurrPubKey = MineKeys[0];
+        if (accountKeys.Count > 0){
+            CurrPubKey = accountKeys[0];
+        } else {
+            AddPubKeyToList("Bob");
+            AddPubKeyToList("Alice");
+            AddPubKeyToList("Charlie");
+            AddPubKeyToList("Dave");
+            AddPubKeyToList("Eve");
+            AddPubKeyToList("Ferdie");
+            // CurrPubKeyText.text = "请先生成一个账号";
+            // CurrPubKey = "空";
         }
-        else {
-            CurrPubKeyText.text = "请先生成一个账号";
-            CurrPubKey = "空";
-        }
+        CurrPubKey = accountKeys[0];
     }
 
     public void ChangeDropKey() {
-        CurrPubKeyText.text = MineKeys[dropdown.value];
+        CurrPubKeyText.text = accountKeys[dropdown.value];
         CurrPubKey = CurrPubKeyText.text;
     }
 
@@ -59,42 +64,42 @@ public class HttpPolka : MonoBehaviour
     public void StartMintEvent()
     {
         mintname = inputMintName.text;
-        StartCoroutine(IMintEvent(mintname));
+        StartCoroutine(IMintEvent(CurrPubKey,mintname));
     }
     /// <summary>
     /// 添加生成出来的公钥
     /// </summary>
     /// <param name="key"></param>
     public void AddPubKeyToList(string key) {
-        if (!MineKeys.Contains(key)) {
-            MineKeys.Add(key);
-            if (MineKeys.Count == 1) {
-                CurrPubKeyText.text = MineKeys[0];
+        if (!accountKeys.Contains(key)) {
+            accountKeys.Add(key);
+            if (accountKeys.Count == 1) {
+                CurrPubKeyText.text = accountKeys[0];
                 CurrPubKey = CurrPubKeyText.text;
             }
 
-            count.text = MineKeys.Count.ToString();
+            count.text = accountKeys.Count.ToString();
 
             List<Dropdown.OptionData> listOptions = new List<Dropdown.OptionData>();
             listOptions.Add(new Dropdown.OptionData(key));
             dropdown.AddOptions(listOptions);
-            Debug.Log("目前有"+MineKeys.Count+"个公钥");
+            Debug.Log("目前有"+accountKeys.Count+"个公钥");
         }
     }
 
-    IEnumerator IMintEvent(string name) {
-        string url = "http://localhost:5000/swallower/mintSwallower?name="+name;
+    IEnumerator IMintEvent(string accountName,string name) {
+        string url = "http://localhost:5000/swallower/mintSwallower?account="+accountName+"&name="+name;
         WWW www = new WWW(url);
         yield return www;
         if (!www.isDone)
         {
             Debug.Log("错误");
-            mintMesInput.text = "Mint失败--"+www.error;
+            chainMessageZone.text = "Mint失败--"+www.error;
             MintText.text = "Mint失败--" + www.error;
         }
         else {
             MintText.text = "Mint结果-"+www.text;
-            mintMesInput.text = "Mint结果-" + www.text;
+            chainMessageZone.text = "Mint结果-" + www.text;
         }
     }
 }
